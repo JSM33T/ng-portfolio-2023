25f6a1466aff9fb0dfc0198c309fa017d2edcc5b
jsm33t <jskainthofficial@gmail.com>
2024-06-15 13:23:29 +0530

    fix providers

--- Almondcove.UI/src/app/app.module.ts
+++ Almondcove.UI/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
-import { GoogleLoginProvider, GoogleSigninButtonModule, SocialAuthServiceConfig, SocialLoginModule } from '@abacritt/angularx-social-login';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
@@ -16,25 +15,10 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    LoadingBarRouterModule,
-    SocialLoginModule,
-    GoogleSigninButtonModule
+    LoadingBarRouterModule
   ],
   providers: [
-    {
-      provide: 'SocialAuthServiceConfig',
-      useValue: {
-        autoLogin: false,
-        providers: [
-          {
-            id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider(
-              '881148390473-ctplpemr784g2j7e8vf6kh6m2ffd1obq.apps.googleusercontent.com'
-            )
-          }
-        ]
-      } as SocialAuthServiceConfig,
-    }
+  
   ],
   bootstrap: [AppComponent]
 })
